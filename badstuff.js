a8425a8a86f1c4faf4a509716b885b2792318212
Vizreki <122842805+Vizreki@users.noreply.github.com>
2023-05-22 23:17:24 -0500

    Alpha Version
    Most mechanics are in place and ready for testing to get the game balanced. It's wildly unbalanced right now.

--- badstuff.js
+++ badstuff.js
@@ -4,85 +4,108 @@
 //BadChecker >>>> CrimeResolver / SickResolver / DamageResolver
 
 let allgood = [
-    "All systems operational.",
-    "Everything is running smoothly.",
-    "Visitors and staff are enjoying the station.",]
+    "Report: All systems operational.",
+    "Report: Everything is running smoothly.",
+    "Report: Visitors and staff are enjoying the station.",]
 
-let genericmultiwarning = "Multiple departments are in need of personnel."
-
-let crimeandsick = "Security and Medical are both reporting insufficient staff. Conditions are deteriorating."
-let crimeanddamage = "Security and Maintenance are both reporting insufficient staff. The station is deteriorating rapidly."
-let damageandsick = "Medical and Maintenance are both reporting insufficient staff. Conditions are deteriorating."
+let genericwarning = "Report: One or more departments could use additional staff."
 
 let crimelvl1 = [
-    "Security is reporting that theft is on the rise.",
-    "Reports of fights and thefts are increasing.",
-    "Some areas of the station are getting unruly.",]
+    "Report: Security is reporting that theft is on the rise.",
+    "Report: Fights and theft seem to be increasing.",
+    "Report: Visitors are avoiding parts of the station due to crime.",]
 
 let crimelvl2 = [
-    "Crime is getting out of hand. Hire additional security personnel.",
-    "Visitors and staff are being targeted by criminals often. Hire additional security personnel.",
-    "There was a ship-board robbery at the station yesterday. Hire additional security personnel."]
+    "Report: Crime is getting out of hand. Hire additional security personnel.",
+    "Report: Visitors and staff are being targeted by criminals often. Hire additional security personnel.",
+    "Report: There was a ship-board robbery at the station yesterday. Hire additional security personnel."]
 
 let crimelvl3 = [
-    "Crime on the station is getting out of control. Hire security personnel immediately.",
-    "There is a shootout near the upper level lounge. Hire security personnel immediately.",
-    "Pirates are openly taking what they want and killing travelers. Hire security personnel immediately."]
+    "Report: Crime on the station is out of control. Hire security personnel immediately.",
+    "Report: There is a shootout near the upper level lounge. Hire security personnel immediately.",
+    "Report: Pirates are openly taking what they want and killing travelers. Hire security personnel immediately."]
 
 let sicklvl1 = [
-    "There are very long lines at the clinic.",
-    "The clinic is requesting additional staff.",
-    "The Medical Director has requested another doctor to open a new clinic.",]
+    "Report: There are very long lines at the clinic.",
+    "Report: The clinic is requesting additional staff.",
+    "Report: The Medical Director has requested another doctor to open a new clinic.",]
 
 let sicklvl2 = [
-    "Quite a few people are sick and cannot get treatment. Hire Medical staff.",
-    "A visitor died today because he didn't receive first aid in time. Hire Medical staff.",
-    "The sick and injured are having to leave the station. Hire Medical staff."]
+    "Report: Quite a few people are sick and cannot get treatment. Hire Medical staff.",
+    "Report: A visitor died today because he didn't receive first aid in time. Hire Medical staff.",
+    "Report: The sick and injured are having to leave the station. Hire Medical staff."]
 
 let sicklvl3 = [
-    "Several patients are in critical condition without proper care. Inrease Healthcare workers immediately.",
-    "A strange disease has begun to spread. Hire additional medical staff immediately.",
-    "Multiple fatalities have been reported over the past week. Inrease Healthcare workers immediately."]
+    "Report: Several patients are in critical condition without proper care. Inrease Healthcare workers immediately.",
+    "Report: A strange disease has begun to spread. Hire additional medical staff immediately.",
+    "Report: Multiple fatalities have been reported over the past week. Inrease Healthcare workers immediately."]
     
 let damagelvl1 = [
-    "Wear and tear on the station is getting excessive. Time for some repairs.",
-    "A ship has been inoperable for over several days. Increase maintenance staff.",
-    "Visitors and staff are complaining about broken machinery and services.",]
+    "Report: Wear and tear on the station is getting excessive. Time for some repairs.",
+    "Report: A ship has been inoperable for over several days. Increase maintenance staff.",
+    "Report: Visitors and staff are complaining about broken machinery and services.",]
 
 let damagelvl2 = [
-    "Visitors are beginning to avoid the station due to unsafe conditions. Increase maintennace staff.",
-    "Life Support in the cafeteria has failed. The area is being evacuated.",
-    "A fuel pump exploded earlier, causing several injuries. Additional maintenance personnel required."]
+    "Report: Visitors are beginning to avoid the station due to unsafe conditions. Increase maintennace staff.",
+    "Report: Life Support in the cafeteria has failed. The area is being evacuated.",
+    "Report: A fuel pump exploded earlier, causing several injuries. Additional maintenance personnel required."]
 
 let damagelvl3 = [
-    "Many services are offline and Dock A is barely functional. Increase maintenance immediately.",
-    "The station is at risk of significant damage or destruction due to multiple hazards.",
-    "There are multiple unsafe conditions putting visitors and staff at risk. Increase maintenance immediately."]
+    "Report: Many services are offline and Dock A is barely functional. Increase maintenance immediately.",
+    "Report: The station is at risk of significant damage or destruction due to multiple hazards.",
+    "Report: There are multiple unsafe conditions putting visitors and staff at risk. Increase maintenance immediately."]
   
-//document.getElementById("status").innerHTML = allgood[Math.floor(Math.random() * allgood.length)];
+function Theft1(){stolen = vis * Math.floor(Math.random() * 8) + 4;cred -= stolen;}
+function Theft2(){stolen = vis * Math.floor(Math.random() * 10) + 5;cred -= stolen;}
+function Theft3(){stolen = vis * Math.floor(Math.random() * 12) + 10;cred -= stolen;}
 
+function Sickness1(){vis -= Math.floor(vis * .2);}
+function Sickness2(){vis -= Math.floor(vis * .4);}
+function Sickness3(){vis -= Math.floor(vis * .6);}
 
-function badChecker(){
+function Damages1(){vis -= Math.floor(vis * .1);glam--;}
+function Damages2(){vis -= Math.floor(vis * .25);glam--;}
+function Damages3(){vis -= Math.floor(vis * .5);glam-= 2;}
+
+function BadChecker(){
     if (crime > 1 || sick  > 1 || damage >1){
         let worst = Math.max(crime, sick, damage);
         if (worst === crime){
-            switch (crime){
-                case 2:
-                case 3:
-                    document.getElementById("status").innerHTML = crimelvl1[Math.floor(Math.random() * crimelvl1.length)];
+            if (crime >= 2 && crime <= 3){
+            document.getElementById("status").innerHTML = crimelvl1[Math.floor(Math.random() * crimelvl1.length)];Theft1();
+            } else if (crime >= 4 && crime <= 7){
+                document.getElementById("status").innerHTML = crimelvl2[Math.floor(Math.random() * crimelvl2.length)];
+                document.getElementById("status").style.color = "rgb(255, 255, 0)";Theft2();
+            } else if (crime >= 8){
+                document.getElementById("status").innerHTML = crimelvl3[Math.floor(Math.random() * crimelvl3.length)];
+                document.getElementById("status").style.color = "rgb(255, 0, 0)";Theft3();}}
+        
+        if (worst === sick){
+            if (sick >= 2 && sick <= 3){
+            document.getElementById("status").innerHTML = sicklvl1[Math.floor(Math.random() * sicklvl1.length)];Sickness1();
+            } else if (sick >= 4 && sick <= 7){
+                document.getElementById("status").innerHTML = sicklvl2[Math.floor(Math.random() * sicklvl2.length)];
+                document.getElementById("status").style.color = "rgb(255, 255, 0)";Sickness2();
+            } else if (sick >= 8){
+                document.getElementById("status").innerHTML = sicklvl3[Math.floor(Math.random() * crimelvl3.length)];
+                document.getElementById("status").style.color = "rgb(255, 0, 0)";Sickness3();}}
+                
+        if (worst === damage){
+            if (damage >= 2 && damage <= 3){
+            document.getElementById("status").innerHTML = damagelvl1[Math.floor(Math.random() * damagelvl1.length)];Damages1();
+            } else if (damage >= 4 && damage <= 7){
+                document.getElementById("status").innerHTML = damagelvl2[Math.floor(Math.random() * damagelvl2.length)];
+                document.getElementById("status").style.color = "rgb(255, 255, 0)";Damages2();
+            } else if (damage >= 8){
+                document.getElementById("status").innerHTML = damagelvl3[Math.floor(Math.random() * damagelvl3.length)];
+                document.getElementById("status").style.color = "rgb(255, 0, 0)";Damages3();}}
+
             }
-        }
+
+    else if (crime === 1 || sick  === 1 || damage === 1){
+        {document.getElementById("status").innerHTML = genericwarning;}
+    } else document.getElementById("status").innerHTML = allgood[Math.floor(Math.random() * allgood.length)]        
     }
 
 
-
-
-
-
-
-
-
-
-    else {document.getElementById("status").innerHTML = allgood[Math.floor(Math.random() * allgood.length)];}
-    setTimeout(badChecker, 20000)
-}
+    
--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-let cred = 50000;
+let cred = 5000;
 let fuel = 100;
 let habs = 1;
 let goods = 50;
 let ships = 0;
-let pop = 1;
+let pop = 0;
 let newh = 0;
 let vis = 0;
+let apt = 0;
 let docks = 1;
 let adm = 1;
 let sec = 0;
@@ -14,15 +15,16 @@ let log = 0;
 let mer = 0;
 let mnt = 0;
 
-let started = false;
-
 let statusmessage = "";
 
-let hiringInterval = 60000;
+let started = false;
+
+let hiringInterval = 40000;
 
 let tstaff = 1;
 let maxcap = habs * 6;
-let maxvis = docks * 300;
+let maxvis = docks * 100;
+let maxres = apt * 30;
 
 let glam = 1;
 
@@ -39,23 +41,34 @@ document.addEventListener('DOMContentLoaded', function() {var input = document.g
 window.onload = function() {
   newName = localStorage.getItem('newName') || '';
   document.getElementById("title").innerHTML = newName + " Station";
-  document.getElementById("status").innerHTML = allgood[Math.floor(Math.random() * allgood.length)];
   DisplayNums();
+  music();
+}
+
+function onStart(){
+  BadChecker();
   Intervals();
+  Population();
   setTimeout(VisGen, 10000);
   setTimeout(VisLeave, 10000);
   setTimeout(GoodsEcon, 10000);
   setTimeout(FuelEcon, 10000);
-  setTimeout(AddNewh, 1000)
-  music();
-  badChecker();
+  setTimeout(AddNewh, 5000)
+  setTimeout(PayRoll, 29000)
+  setTimeout(StaffWork, 10000)
+  delStart();
+  badTracker();
+  started = true;
 }
 
 function Intervals(){
   setInterval(DisplayNums, 100)
-  setInterval(PayRoll, 29000)
-  setInterval(StaffWork, 10000)
+  setInterval(BadChecker, 10000)
 }
+
+function badTracker(){
+  {console.log("Crime: " + crime)}{console.log("Sick: " + sick)}{console.log("Dmg: " + damage)}
+  setTimeout(badTracker, 5000)}
 
 //UI Updater
 function DisplayNums(){
@@ -69,8 +82,9 @@ function DisplayNums(){
     document.getElementById("hb").innerHTML = "Habs: " + habs;
     document.getElementById("gd").innerHTML = "Goods: " + goods;
     document.getElementById("sh").innerHTML = "Ships: " + ships;
+    document.getElementById("ap").innerHTML = "Apartments: " + apt;
     document.getElementById("dk").innerHTML = "Docks: " + docks;
-    document.getElementById("po").innerHTML = "Population: " + pop;
+    document.getElementById("po").innerHTML = "Residents: " + pop;
     document.getElementById("nh").innerHTML = "Applicants: " + newh;
     document.getElementById("vi").innerHTML = "Visitors: " + commaVis;
     document.getElementById("ad").innerHTML = "Admin: " + adm;
@@ -80,8 +94,19 @@ function DisplayNums(){
     document.getElementById("mr").innerHTML = "Merchants: " + mer;
     document.getElementById("mt").innerHTML = "Maintenance: " + mnt;
     Updater();
-    
-    }
+    if (tstaff >= maxcap){document.getElementById("nh").style.color = "rgb(80,80,80)";}
+    else if (tstaff < maxcap)document.getElementById("nh").style.color = "rgb(255,255,255)";
+
+    if (fuel === 0){document.getElementById("fl").style.color = "rgb(255,0,0)";}
+    else document.getElementById("fl").style.color = "rgb(255,255,255)";
+
+    if (goods === 0){document.getElementById("gd").style.color = "rgb(255,0,0)";}
+    else document.getElementById("gd").style.color = "rgb(255,255,255)";
+
+    if (vis === maxvis){document.getElementById("dk").style.color = "rgb(255,255,0)";}
+    else document.getElementById("dk").style.color = "rgb(255,255,255)";
+
+}
     
 function Updater(){
   tstaff = adm + sec + med + log + mer + mnt;
@@ -90,20 +115,29 @@ function Updater(){
   if (vis < 0) {vis = 0};
   if (vis > maxvis){vis = maxvis}};
 
-function PayRoll(){cred -= tstaff * 500;blip();}
+function PayRoll(){cred -= tstaff * 500;blip();setTimeout(PayRoll, 29000)}
 
 function VisGen() {
-  if (vis <= maxvis && glam >= 1 && glam <= 5) {
+  if (vis <= maxvis && glam >= 1 && glam <= 3) {
     const randomIncrement = Math.floor(Math.random() * 6) + 2;
     vis += randomIncrement;
-  } else if (vis <= maxvis && glam >= 6 && glam <= 10) {
-    const randomIncrement = Math.floor(Math.random() * 10) + 12; 
+  } else if (vis <= maxvis && glam >= 4 && glam <= 6) {
+    const randomIncrement = Math.floor(Math.random() * 10) + 5; 
     vis += randomIncrement;
-  } else if (vis <= maxvis && glam > 11 && glam <= 15) {
-    const randomIncrement = Math.floor(Math.random() * 22) + 22;
+  } else if (vis <= maxvis && glam >= 7 && glam <= 14) {
+    const randomIncrement = Math.floor(Math.random() * 10) + 10;
     vis += randomIncrement;
-  } else if (vis <= maxvis && glam >= 16) {
-    const randomIncrement = Math.floor(Math.random() * 20) + 32;
+  } else if (vis <= maxvis && glam >= 15 && glam <= 25) {
+    const randomIncrement = Math.floor(Math.random() * 15) + 20;
+    vis += randomIncrement;
+  } else if (vis <= maxvis && glam >=26  && glam <= 50) {
+    const randomIncrement = Math.floor(Math.random() * 20) + 30;
+    vis += randomIncrement;
+  } else if (vis <= maxvis && glam >= 51 && glam <= 70) {
+    const randomIncrement = Math.floor(Math.random() * 28) + 45;
+    vis += randomIncrement;
+  } else if (vis <= maxvis && glam >= 71) {
+    const randomIncrement = Math.floor(Math.random() * 35) + 60;
     vis += randomIncrement;
   }
   const randomInterval = Math.floor(Math.random() * (12000 - 5000 + 1)) + 5000;
@@ -111,34 +145,45 @@ function VisGen() {
 }
 
 function VisLeave() {
-    if (glam >= 1 && glam <= 5) {
-      const randomIncrement = Math.floor(Math.random() * 6) + 1;
-      vis -= randomIncrement;
-    } else if (glam >= 6 && glam <= 10) {
-      const randomIncrement = Math.floor(Math.random() * 8) + 8; 
-      vis -= randomIncrement;
-    } else if (glam >= 11 && glam <= 15) {
-      const randomIncrement = Math.floor(Math.random() * 20) + 20;
-      vis -= randomIncrement;
-    } else if (glam >= 16) {
-      const randomIncrement = Math.floor(Math.random() * 20) + 30;
-      vis -= randomIncrement;
-    } else if (glam < 0) {vis = Math.floor(Math.random() * 14) + 1;}
+    if (glam >= 1 && glam <= 3) {
+      const randomIncrement = Math.floor(Math.random() * 6) + 2;
+    vis -= randomIncrement;
+  } else if (vis <= maxvis && glam >= 4 && glam <= 6) {
+    const randomIncrement = Math.floor(Math.random() * 9) + 4; 
+    vis -= randomIncrement;
+  } else if (vis <= maxvis && glam >= 7 && glam <= 14) {
+    const randomIncrement = Math.floor(Math.random() * 9) + 9;
+    vis -= randomIncrement;
+  } else if (vis <= maxvis && glam >= 15 && glam <= 25) {
+    const randomIncrement = Math.floor(Math.random() * 13) + 18;
+    vis -= randomIncrement;
+  } else if (vis <= maxvis && glam >=26  && glam <= 50) {
+    const randomIncrement = Math.floor(Math.random() * 18) + 28;
+    vis -= randomIncrement;
+  } else if (vis <= maxvis && glam >= 51 && glam <= 70) {
+    const randomIncrement = Math.floor(Math.random() * 21) + 38;
+    vis -= randomIncrement;
+  } else if (vis <= maxvis && glam >= 71) {
+    const randomIncrement = Math.floor(Math.random() * 28) + 45;
+    vis -= randomIncrement;
+  } else if (glam < 0) {vis = Math.floor(Math.random() * 14) + 1;}
   const randomInterval = Math.floor(Math.random() * (16000 - 11000 + 1)) + 11000;
   setTimeout(VisLeave, randomInterval);
 }
 
 function StaffWork(){
 goods += log * 5;
-goods += mer * 20;
+goods += mer * Math.floor(vis * .25);
+cred += ships * (vis * 4)
+if (goods > 0){cred += vis * (mer * 15)}
+setTimeout(StaffWork, 14000)
 }
 
 function GoodsEcon(){
   if (goods > 0){
   let multiplier = Math.floor(Math.random() * (1.8 - 0.08)) + 0.05;
   let randCred = Math.floor(vis * multiplier);
-  cred += randCred + 100 + (100 * mer);
-  console.log(randCred);
+  cred += randCred + 200 + (180 * mer);
   goods -= Math.floor(randCred * .5);}
   if (goods < 0) {goods = 0};
   setTimeout(GoodsEcon, 4000);
@@ -150,34 +195,41 @@ function FuelEcon(){
   let fuelAmount = Math.floor(Math.random() * 5);
   if (fuelBuy > 0 && fuel >= (fuelAmount * 10)){
   fuel -= Math.floor(fuelAmount * 10);
-  cred += Math.floor(fuelAmount * 350)}}
+  cred += Math.floor(fuelAmount * 400)}}
   if (fuel < 0) {fuel = 0;}
   setTimeout(FuelEcon, 7000);
 }
 
 //New Hire Generator
-function AddNewh(){if (newh < 3 && tstaff < maxcap)newh++;setTimeout(AddNewh, hiringInterval)}
+function AddNewh(){if (newh < 3 && tstaff < maxcap)newh++;
+  setTimeout(AddNewh, hiringInterval)}
+
+function Population(){if (vis > 50 && pop < maxres - 4) {pop+= Math.floor(Math.random() * 4) + 1;}
+    cred += 350 * pop;
+    setTimeout(Population, 20000)
+}
 
 //Staff Assignment Buttons
-function AsgnAdm(){if (newh !== 0 && tstaff < maxcap){adm++;newh--;hiringInterval -= 5000;crime--;damage--;admSound();}}
-function AsgnSec(){if (newh !== 0 && tstaff < maxcap){sec++;newh--;crime -= 20;secSound();}}
-function AsgnMed(){if (newh !== 0 && tstaff < maxcap){med++;newh--;sick -= 3;glam++;medSound();}}
-function AsgnLog(){if (newh !== 0 && tstaff < maxcap){log++;newh--;damage--;logSound();}}
-function AsgnMer(){if (newh !== 0 && tstaff < maxcap){mer++;newh--;glam++;merSound();}}
-function AsgnMnt(){if (newh !== 0 && tstaff < maxcap){mnt++;newh--;damage -= 3;mntSound();}}
+function AsgnAdm(){if (newh !== 0 && tstaff < maxcap && started){adm++;newh--;hiringInterval-= 10000;crime--;damage--;admSound();}}
+function AsgnSec(){if (newh !== 0 && tstaff < maxcap&& started){sec++;newh--;crime -= 20;secSound();}}
+function AsgnMed(){if (newh !== 0 && tstaff < maxcap&& started){med++;newh--;sick -= 3;glam++;medSound();}}
+function AsgnLog(){if (newh !== 0 && tstaff < maxcap&& started){log++;newh--;damage--;logSound();}}
+function AsgnMer(){if (newh !== 0 && tstaff < maxcap&& started){mer++;newh--;glam++;merSound();}}
+function AsgnMnt(){if (newh !== 0 && tstaff < maxcap&& started){mnt++;newh--;damage -= 3;mntSound();}}
 
-function BuildHab(){if (cred >= 10000){{habs++;cred-= 10000;glam++;sick++;habSound();}}}
-function BuildLounge(){if (cred >= 5000){{loungeSound();cred-= 5000;glam+= 2;}}}
-function BuildDock(){if (cred >= 15000){{docks++;cred-= 15000;glam += 3; crime +=3;sick++;damage+=2;dockSound();}}}
-function BuyFuel(){if (cred >= -5000){{fuel+= 100;cred-= 1000;fuelSound();}}}
-function BuyShip(){if (cred >= 20000){{ships++;cred-= 20000;glam += 8;crime++;damage += 3;shipSound();}}}
+function BuildHab(){if (cred >= 10000 && started){{habs++;cred-= 10000;sick++;habSound();}}}
+function BuildApartment(){if (cred >= 35000 && started){{apt++;cred-= 35000;sick+= 2;crime+= 2;damage+= 1;habSound();}}}
+function BuildLounge(){if (cred >= 5000 && started){{loungeSound();cred-= 5000;sick++;glam+= 2;}}}
+function BuildDock(){if (cred >= 15000 && started){{docks++;cred-= 15000;glam += 2; crime +=2;sick++;damage+=2;dockSound();}}}
+function BuyFuel(){if (cred >= -5000 && started){{fuel+= 100;cred-= 1000;fuelSound();}}}
+function BuyShip(){if (cred >= 20000 && started){{ships++;cred-= 20000;glam += 4;crime+= 2;damage += 3;shipSound();}}}
 
-function fireAdm(event) {if (event.key === 'Delete') {if (adm > 0)adm--;fired();}}
-function fireSec(event) {if (event.key === 'Delete') {if (sec > 0)sec--;fired();}}
-function fireMed(event) {if (event.key === 'Delete') {if (med > 0)med--;fired();}}
-function fireLog(event) {if (event.key === 'Delete') {if (log > 0)log--;fired();}}
-function fireMer(event) {if (event.key === 'Delete') {if (mer > 0)mer--;fired();}}
-function fireMnt(event) {if (event.key === 'Delete') {if (mnt > 0)mnt--;fired();}}
+function fireAdm(event) {if (event.key === 'Delete' && started) {if (adm > 0)adm--;fired();}}
+function fireSec(event) {if (event.key === 'Delete' && started) {if (sec > 0)sec--;fired();}}
+function fireMed(event) {if (event.key === 'Delete' && started) {if (med > 0)med--;fired();}}
+function fireLog(event) {if (event.key === 'Delete' && started) {if (log > 0)log--;fired();}}
+function fireMer(event) {if (event.key === 'Delete' && started) {if (mer > 0)mer--;fired();}}
+function fireMnt(event) {if (event.key === 'Delete' && started) {if (mnt > 0)mnt--;fired();}}
 
 function Rename() {
   blip();
@@ -186,3 +238,5 @@ function Rename() {
   document.getElementById("myInput").value = "";
   localStorage.setItem('newName', newName);
 }
+
+function delStart(){document.getElementById("startbutton").remove();}
